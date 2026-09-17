0020778fefc540099ed92ab4683b3c6435cff676
Sachin <kunwarsachin003@gmail.com>
2026-09-17 23:38:24 +0545

    Create Product model with owner relationship

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product
+# Register your models here
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=(
+        "id","name","price","stock","owner","created_at",
+    )
+
+    list_filter=(
+        "owner",
+    )
+
+    search_fields=(
+        "name",
+        "description",
+    )
+
+
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    stock = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
