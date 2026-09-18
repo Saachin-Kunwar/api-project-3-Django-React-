f2b7ab305ad6c11d30b6e54a55e12cf5a48f7023
Sachin <kunwarsachin003@gmail.com>
2026-09-18 12:13:45 +0545

    Add session login API

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
-from .serializers import RegisterSerializer
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import generics
-# Create your views here.
+from .serializers import RegisterSerializer
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class= RegisterSerializer
 
+
+class LoginView(APIView):
+    def post(self,request):
+        username= request.data.get("username")
+        password= request.data.get("password")
+
+        user= authenticate(
+            username=username,
+            password=password
+        )
+        if user is None:
+            return Response(
+                {"detail": "Invalid username or password!"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        login(request,user)
+
+        return Response(
+            {
+                "detail":"Login successful.",
+                "username":user.username
+            },
+            status= status.HTTP_200_OK
+        )
