0f3c63e551691e2ed749219b08760f9f3594d1cf
Sachin <kunwarsachin003@gmail.com>
2026-09-18 21:21:29 +0545

    Add action based permissions

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,35 +1,55 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from rest_framework import generics
 from .serializers import RegisterSerializer
 
 
 class RegisterView(generics.CreateAPIView):
-    serializer_class= RegisterSerializer
+    serializer_class = RegisterSerializer
 
 
 class LoginView(APIView):
-    def post(self,request):
-        username= request.data.get("username")
-        password= request.data.get("password")
 
-        user= authenticate(
+    def post(self, request):
+
+        username = request.data.get("username")
+        password = request.data.get("password")
+
+        user = authenticate(
             username=username,
             password=password
         )
+
         if user is None:
             return Response(
-                {"detail": "Invalid username or password!"},
+                {"detail": "Invalid username or password."},
                 status=status.HTTP_400_BAD_REQUEST
             )
-        login(request,user)
+
+        login(request, user)
 
         return Response(
             {
-                "detail":"Login successful.",
-                "username":user.username
+                "detail": "Login successful.",
+                "username": user.username
             },
-            status= status.HTTP_200_OK
+            status=status.HTTP_200_OK
+        )
+
+
+class LogoutView(APIView):
+
+    def post(self, request):
+
+        logout(request)
+
+        return Response(
+            {
+                "detail": "Logout successful."
+            },
+            status=status.HTTP_200_OK
         )
--- backend/products/views.py
+++ backend/products/views.py
@@ -10,18 +10,66 @@
 #     def perform_create(self,serializer):
 #         serializer.save(owner=self.request.user)
 
+# from rest_framework.viewsets import ModelViewSet
+# from rest_framework.authentication import SessionAuthentication
+# from rest_framework.permissions import IsAuthenticated
+# from .models import Product
+# from .serializers import ProductSerializer
+
+# class ProductViewSet(ModelViewSet):
+#     queryset= Product.objects.all()
+#     serializer_class= ProductSerializer
+
+#     authentication_class= [SessionAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+
+# custom permission
+# from rest_framework.viewsets import ModelViewSet
+# from rest_framework.authentication import SessionAuthentication
+# from rest_framework.permissions import IsAuthenticated
+
+# from .models import Product
+# from .serializers import ProductSerializer
+# from .permissions import IsOwner
+
+# class ProductViewSer(ModelViewSet):
+#     queryset= Product.objects.all()
+#     serializer_class= ProductSerializer
+
+#     authentication_classes=[SessionAuthentication]
+
+#     permission_classes=[
+#         IsAuthenticated,
+#         IsOwner,
+#     ]
+
+#     def perform_create(self,serializer):
+#         serializer.save(owner=self.request.user)
+        
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsOwner
 
-class ProductViewSet(ModelViewSet):
+class ProductViewSer(ModelViewSet):
     queryset= Product.objects.all()
-    serializer_class= ProductSerializer
+    serializer_class = ProductSerializer
 
-    authentication_class= [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
 
-    def perform_create(self, serializer):
+    def get_permissions(self):
+        if self.action in ["update", "partial_update", "destroy"]:
+            permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            permission_classes =[IsAuthenticated]
+
+        return [permission() for permission in permission_classes]
+    
+    def perform_create(self,serializer):
         serializer.save(owner=self.request.user)
