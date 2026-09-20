b59631fb51f526e82584ee9c57d24a56f02e9083
Sachin <kunwarsachin003@gmail.com>
2026-09-20 11:57:36 +0545

    Integrate JWT protected production API with React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,51 @@
-// import { useEffect, useState } from "react";
-// import API_BASE_URL from "./services/api";
-
-// function App() {
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     fetch(`${API_BASE_URL}/products/`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log(data);
-//         setProducts(data);
-//       })
-//       .catch((error) => {
-//         console.error("API Error:", error);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>ProductHub</h1>
-
-//       {products.map((product) => (
-//         <div key={product.id}>
-//           <h2>{product.name}</h2>
-//           <p>Price: {product.price}</p>
-//           <p>Stock: {product.stock}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default App;
-
+import { useEffect, useState } from "react";
 import Login from "./pages/Login";
-function App(){
+import API_BASE_URL from "./services/api";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("access"); //browser bata JWT nikalxa
+
+    fetch(`${API_BASE_URL}/products/`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Products:", data);
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Product API Error:", error);
+      });
+  }, []);
+
   return (
     <div>
       <Login />
+
+      <hr />
+
+      <h1>Products</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Price: {product.price}</p>
+          <p>Stock: {product.stock}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App;
